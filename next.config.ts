69db41f5d31877c7f92533ecf566fa6ea799db75
Sobhan Tahmasebi <niinja2017@gmail.com>
2026-08-15 23:11:44 +0330

    setup GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,20 @@
 import type { NextConfig } from "next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const bundleAnalyzer = withBundleAnalyzer({
+    enabled: process.env.ANALYZE === "true",
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+
+    basePath: "/technolife",
+
+    images: {
+        unoptimized: true,
+    },
+
+    trailingSlash: true,
 };
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === "true",
-})
-
-module.exports = withBundleAnalyzer(nextConfig)
+export default bundleAnalyzer(nextConfig);
